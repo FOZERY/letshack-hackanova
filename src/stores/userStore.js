f5d172bbd8d98e98afb624b0e78702ebfafd4ce5
fozery <fozery@yandex.ru>
2024-05-25 18:36:02 +0300

    feat: change userStore

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -2,8 +2,18 @@ import { defineStore } from 'pinia';
 
 const userData = {
     id: 1,
+    name: 'Дмитрий',
+    surname: 'Тагиев',
+    birthday: '02/09/2004',
+    city: 'Белгород',
     email: 'email@test.ru',
-    phone: 'phone',
+    phone: '+7 (904) 080 64-56',
+    telegramUrl: 't.me/FOZERY',
+    searchingJob: true,
+    hackExperience: 'Нет опыта участия в хакатонах',
+    jobExperience: '',
+    education: 'Студент 2 курса IT-специальности',
+    about: 'Я студент 2 курса специальности CS. Знаю HTML, CSS, JavaScript, делал проект на Vue.js и PHP, немного изучал C/C++, знаком с Docker, Git\n',
     requestCommand: true,
     tags: [
         {
