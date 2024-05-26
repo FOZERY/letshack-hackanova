6df31779582760327c81dc7e85e3db53f60cee60
fozery <fozery@yandex.ru>
2024-05-26 17:23:41 +0300

    feat: add modal

--- src/components/SearchStatus.vue
+++ src/components/SearchStatus.vue
@@ -1,7 +1,9 @@
 <script setup></script>
 
 <template>
-    <div class="search_status border rounded-2xl body-text-medium py-2 px-3">
+    <div
+        class="search_status border rounded-2xl body-text-medium py-2 px-3 select-none"
+    >
         <slot />
     </div>
 </template>
